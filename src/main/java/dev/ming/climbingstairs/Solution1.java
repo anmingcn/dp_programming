ce2d61e281f1d1d3212eaa38065b08f8d610c26a
anmingcn <165182557+anmingcn@users.noreply.github.com>
2025-10-16 20:08:42 +0200

    Add comment to print statement in Solution1

--- src/main/java/dev/ming/climbingstairs/Solution1.java
+++ src/main/java/dev/ming/climbingstairs/Solution1.java
@@ -3,7 +3,7 @@ package dev.ming.climbingstairs;
 public class Solution1 {
 
   public static void main(String[] args) {
-        System.out.println("solution 1 running");
+        System.out.println("solution 1 running"); // just test
     }
   
 }
